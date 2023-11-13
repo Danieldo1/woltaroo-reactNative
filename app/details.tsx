98f5de77b5d12d0e9b43a17350b20f5b999162d1
Dan <daniel.speranskiy@live.com>
2023-11-13 08:27:50 +0400

    Added section list for dishes

--- app/details.tsx
+++ app/details.tsx
@@ -1,13 +1,33 @@
-import { View, Text,StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { View, Text,StyleSheet, Image, TouchableOpacity, SectionList, ListRenderItem } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import ParallaxScrollView from '@/Components/Parallax'
 import Colors from '@/constants/Colors'
 import { restaurant } from '@/assets/data/restaurant'
-import { useNavigation } from 'expo-router'
+import { Link, useNavigation } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 
 const Details = () => {
     const navigation = useNavigation()
+
+    const DATA= restaurant.food.map((item,index)=>({
+            title: item.category,
+            data: item.meals,
+            index
+    }))
+
+    const renderItem: ListRenderItem<any> = ({item, index}) => (
+      <Link href={'/'} asChild>
+        <TouchableOpacity style={styles.card}>
+          <View style={{flex:1}}>
+            <Text style={styles.cardTxt}>{item.name}</Text>
+            <Text style={styles.cardInfo}>{item.info}</Text>
+            <Text style={styles.cardPrice}>${item.price}</Text>
+          </View>
+            <Image source={item.img} style={styles.img} />
+           
+        </TouchableOpacity>
+      </Link>
+    )
 
     useLayoutEffect(() => {
       navigation.setOptions({
@@ -48,9 +68,21 @@ const Details = () => {
       renderStickyHeader={() => <View key='sticky-header' style={styles.headerStick}>
         <Text style={styles.stickyText}>{restaurant.name}</Text>
       </View>}>
-
         <View style={styles.detailContainer}>
-        <Text>Details</Text>
+        <Text style={styles.restName}>{restaurant.name}</Text>
+        <Text style={styles.restAbout}>{restaurant.about}</Text>
+        <Text style={styles.restTags}>{restaurant.delivery} • {restaurant.tags.map((item, index) => `${item}${index < restaurant.tags.length - 1 ? ' • ' : ''}`)}</Text>
+        
+        <SectionList contentContainerStyle={{paddingBottom: 50}} keyExtractor={(item,index)=> `${index + item.id}`} scrollEnabled={false} sections={DATA} renderItem={renderItem}
+        
+        ItemSeparatorComponent={() => <View style={{height: 1,backgroundColor: Colors.grey,marginHorizontal: 20}}/>}
+        SectionSeparatorComponent={() => <View style={{height: 1,backgroundColor: Colors.grey}}/>}
+        renderSectionHeader={({section:{title,index}}) => (
+          <Text style={styles.sectionHeader}>{title}</Text>
+        )}
+        />
+
+        
         </View>
       </ParallaxScrollView>
     </>
@@ -58,6 +90,44 @@ const Details = () => {
 }
 
 const styles = StyleSheet.create({
+  cardInfo:{
+    fontSize: 14,
+    color: Colors.medium,
+  },
+cardPrice:{
+  paddingVertical: 4
+},
+  cardTxt:{
+    fontWeight: 'bold',
+    fontSize: 16,
+    paddingVertical: 4
+  },
+  img:{
+    height:80,
+    width: 80,
+    borderRadius: 8,
+    marginLeft: 16
+  },
+  card:{
+    backgroundColor: '#fff',
+    padding: 16,
+    flexDirection: 'row',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+  },
+   sectionHeader:{
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 40,
+    margin: 16
+   },
+   restDish:{
+     
+   },
+   
     detailContainer:{
         backgroundColor: Colors.lightGrey,
 
@@ -87,7 +157,28 @@ const styles = StyleSheet.create({
     stickyText:{
         fontSize: 20,
         margin: 10,
+    },
+    restName:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginTop: 16,
+        marginLeft: 16
+    },
+    restTags:{
+        fontSize: 16,
+        marginTop:6,
+        margin: 16,
+        lineHeight: 25,
+        color: Colors.mediumDark
+    },
+    restAbout:{
+       fontSize: 16,
+       marginTop:6,
+        margin: 16,
+        lineHeight: 25,
+        color: Colors.mediumDark
     }
+
 })
 
 export default Details
